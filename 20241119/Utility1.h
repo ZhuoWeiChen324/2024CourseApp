5d044fbe776fb2210ac07966afb7226484f04812
ZhuoWeiChen324 <4b1g0016@stust.edu.tw>
2024-11-26 11:17:16 +0800

    新增 Student 和 Teacher 類別及相關檔案

---
+++ 20241119/Utility1.h
@@ -0,0 +1,21 @@
+#pragma once
+
+using namespace std;
+
+enum class Depertment {
+	ComputerSciece,
+	ElectricalEngineering,
+	ElectronicEngineering,
+	IformationManagement,
+};
+
+enum class ClassName {
+	_1A,
+	_1B,
+	_2A,
+	_2B,
+	_3A,
+	_3B,
+	_4A,
+	_4B,
+};
